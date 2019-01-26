233240cd7fc631e8c1d1e7e99d8f5ac7e62c40bc
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2019-01-26 16:47:51 +0900

    Firebase RDBと連携した

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="ItemList">
-    <h2>ItemList</h2>
-    <!-- todo: for文などを使う -->
-    <p>{{ game1 }}</p>
+    <h2>Board games</h2>
+    <div class="gameList" v-for="(game, index) in games" :key="index">
+      <p class="gameTitle">{{ displayTitle(game) }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,10 +13,28 @@ export default {
   name: "itemlist",
   data() {
     return {
-      game1: "カタン",
-      games: [
-        {"title": ""}
-      ]
+      games: [{name: "", rental: {place: "", status: false}}]
+    }
+  },
+  created: function () {
+    firebase
+      .database()
+      .ref("boardgame/")
+      .once("value")
+      .then(result => {
+        if (result.val()) {
+          const rowGames = result.val();  // [{0: {name: "", rental: {place: "", status: false}}}, ...]
+          let games = []
+          for (const key in rowGames) {
+            games.push(rowGames[key])
+          }
+          this.games = games;
+        }
+      })
+  },
+  methods: {
+    displayTitle: function(gameJson) {
+      return gameJson.name
     }
   }
 }
