e7ac44947266e913e0db5ba31f73efa964743f07
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2019-02-08 12:21:10 +0900

    TensorFlow.js with localhost

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -10,6 +10,15 @@ import * as tf from '@tensorflow/tfjs';
 const x = tf.tensor2d([1, 2, 3, 4], [4, 1])
 const y = tf.tensor2d([1, 3, 5, 7], [4, 1])
 const resultCalc = tf.add(x, y).dataSync()  // JSON like { "0": 2, "1": 5, "2": 8, "3": 11 }
+const model = tf.loadModel('http://localhost:3000/model.json')
+
+model.then(
+  (loadedModel) => {
+    loadedModel.summary()
+    const result = loadedModel.predict(tf.randomUniform([1, 28, 28, 1])).dataSync()
+    console.log(result)
+  }
+ )
 
 let result = []
 for (const key in resultCalc) {
--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -6,6 +6,35 @@ export default {
   props: ['chartdata', 'options'],
   mounted () {
     this.renderChart(this.chartdata, this.options)
+  },
+  data() {
+    return {
+      lights: [{time: 0, value: 0}],
+      chartdata: [],
+      options: []
+    }
+  },
+  beforeCreate: function(){
+    let chartdata = []
+    let options = []
+    firebase
+      .database()
+      .ref("sensor/light")
+      .once("value")
+      .then(result => {
+        if (result.val()) {
+          const rawLights = result.val();  // [{0: {name: "", rental: {place: "", status: false}}}, ...]
+          for (const time in rawLights) {
+            chartdata.push(time)
+            options.push(rawLights[time])
+          }
+        }
+        console.log(chartdata)
+        console.log(options)
+      })
+    
+    this.chartdata = chartdata
+    this.options = options
   }
 }
 </script>
