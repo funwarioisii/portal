0f7e39fe3a8a57041d8b120ffd22e0b241ef0147
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2019-02-24 15:58:53 +0900

    サンプルが動く

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,43 +1,72 @@
 <script>
-import { Line } from 'vue-chartjs'
+// import { Bar } from 'vue-chartjs'
 
-export default {
-  extends: Line,
-  props: ['chartdata', 'options'],
-  mounted () {
-    this.renderChart(this.chartdata, this.options)
-  },
-  data() {
-    return {
-      lights: [{time: 0, value: 0}],
-      chartdata: [],
-      options: []
-    }
-  },
-  beforeCreate: function(){
-    let chartdata = []
-    let options = []
-    firebase
-      .database()
-      .ref("sensor/light")
-      .once("value")
-      .then(result => {
-        if (result.val()) {
-          const rawLights = result.val();  // [{0: {name: "", rental: {place: "", status: false}}}, ...]
-          for (const time in rawLights) {
-            chartdata.push(time)
-            options.push(rawLights[time])
+// let chartdata = {
+//   datacollection: {
+//     labels: [],
+//     datasets: [
+//       {
+//         label: 'Data One',
+//         backgroundColor: '#f87979',
+//         data: []
+//       }
+//     ]
+//   }
+// }
+
+// let options = {
+//   responsive: true,
+//   maintainAspectRatio: false
+// }
+
+// export default {
+//   extends: Bar,
+
+
+//   mounted () {
+//     this.renderChart(chartdata, options)
+//   }
+// }
+import { Bar } from 'vue-chartjs'
+let chartdata = {
+        labels: ["Jan", "Feb"],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [100, 50]
           }
-        }
-        console.log(chartdata)
-        console.log(options)
-      })
-    
-    this.chartdata = chartdata
-    this.options = options
+        ]
+      }
+
+let options = {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+export default {
+  extends: Bar,
+  // beforeCreate: function(){
+  //   firebase
+  //     .database()
+  //     .ref("sensor/light")
+  //     .once("value")
+  //     .then(result => {
+  //       if (result.val()) {
+  //         const rawLights = result.val();  // [{0: {name: "", rental: {place: "", status: false}}}, ...]
+  //         for (const time in rawLights) {
+  //           chartdata.labels.push(time)
+  //           chartdata.datasets[0].data.push(rawLights[time])
+      
+  //         }
+  //       }
+  //     })
+  // },
+
+  mounted() {
+    this.renderChart(
+      chartdata,
+      options
+    )
   }
 }
 </script>
-
-<style>
-</style>
